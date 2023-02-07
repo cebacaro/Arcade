4e7b3813115df1a4f9dda7a1450df5cba0de449e
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-02-07 12:03:14 -0600

    commit on tuesday

--- app.js
+++ app.js
@@ -1,39 +1,72 @@
 const cells = document.querySelectorAll(".cell");
 const startButton = document.getElementById("startButton");
 
-let namePlayer1;
-let namePlayer2;
+let namePlayer1 = "";
+let namePlayer2 = "";
 let player1 = "X";
 let player2 = "O";
-let currentPlayer;
+let currentPlayer = player1;
 
-function renderPlayer() {
-  currentPlayer = currentPlayer === player1 ? player1 : player2;
-  const activePlayer = currentPlayer === player1 ? namePlayer1 : namePlayer2;
-  const activePlayerElement = document.querySelector(
-    `.players[id="${currentPlayer === player1 ? player1 : player2}"]`
-  );
-  activePlayerElement.textContent = activePlayer;
-}
+function setGame() {
+  function getName2() {
+    let nameValue1 = prompt("Player 1, enter your name");
+    namePlayer1 = nameValue1;
+  }
 
-function PlayerName() {
-  namePlayer1 = prompt("Player 1: Please enter your name");
-  namePlayer2 = prompt("Player 2: Please enter your name");
+  function getName1() {
+    nameValue = prompt("Player 2, enter your name");
+    namePlayer2 = nameValue;
+  }
+  getName2();
+  getName1();
   renderPlayer();
 }
 
-function setGame() {
-  const element = document.getElementById(startButton.id);
-  element.textContent = "Let's Play";
+function renderPlayer() {
+  const player1Name = document.getElementById("player1");
+  player1Name.innerText = namePlayer1;
+  const player2Name = document.getElementById("player2");
+  player2Name.innerText = namePlayer2;
 }
-startButton.addEventListener("click", () => {
-  PlayerName();
-});
 
-cells.forEach((cells) => {
-  cells.addEventListener("click", clickEvent);
-});
+function clickCells(event) {
+  if (event.target.textContent) {
+    return;
+  }
+  event.target.textContent = currentPlayer;
+  if (checkWinner(currentPlayer)) {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
 
-function clickEvent() {
-  console.log("hello");
+    startButton.innerText = "winner";
+  } else {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+}
+
+function checkWinner(player) {
+  let winner = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winner.length; i++) {
+    const [a, b, c] = winner[i];
+    if (
+      cells[a].textContent === player &&
+      cells[b].textContent === player &&
+      cells[c].textContent === player
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", clickCells);
 }
